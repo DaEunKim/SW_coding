b0ad989d4ae756e92f82aceae9a87e127ea6e4a3
DaEunKim <ekdms717@naver.com>
2019-03-30 23:26:59 +0900

    3187양치기꿍

---
+++ 3187양치기꿍/3187양치기꿍/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3187양치기꿍
+//
+//  Created by 김다은 on 30/03/2019.
+//  Copyright © 2019 김다은. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
