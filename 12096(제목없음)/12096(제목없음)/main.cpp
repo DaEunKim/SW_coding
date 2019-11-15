78c8f065fbf6e36f4cc046984b3f0620b924eeb2
DaEunKim <ekdms717@naver.com>
2019-11-15 10:17:54 +0900

    12096(제목없음)

---
+++ 12096(제목없음)/12096(제목없음)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  12096(제목없음)
+//
+//  Created by 김다은 on 2019/11/15.
+//  Copyright © 2019 김다은. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+ cout<< "02-521-0487"<<endl;
+}
