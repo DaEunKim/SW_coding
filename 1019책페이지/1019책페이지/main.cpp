a4a6ff0b927f42c48899a91f3c51c075d5597ea8
DaEunKim <ekdms717@naver.com>
2020-04-07 00:36:31 +0900

    add 1019페이지

---
+++ 1019책페이지/1019책페이지/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  1019책페이지
+//
+//  Created by 김다은 on 06/04/2020.
+//  Copyright © 2020 김다은. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+void eachNum(){
+    
+}
+
+int main(int argc, const char * argv[]) {
+    int N;
+    cin >> N;
+    
+    
+}
