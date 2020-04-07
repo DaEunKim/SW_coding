31018d477cfcad835b4b4ddcdff3d5eed43ed11e
DaEunKim <ekdms717@naver.com>
2020-04-07 11:31:18 +0900

    1966프린터큐

---
+++ 1966프린터큐/1966프린터큐/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  1966프린터큐
+//
+//  Created by 김다은 on 2020/04/06.
+//  Copyright © 2020 김다은. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int test_case;
+    cin >> test_case;
+    for(int z = 0;z<test_case;z++){
+        int N, M;
+        cin >> N >> M;
+        
+        
+    }
+}
